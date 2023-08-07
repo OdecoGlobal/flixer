21eece785ab1e74a63cf97e5b04a70bd677aa372
Okechukwu Chidera <okechukwuchidera12@gmail.com>
2023-08-07 17:31:30 +0100

    yeah

--- src/components/NewHero.jsx
+++ src/components/NewHero.jsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
 
-import useSeriesReducer from "../hooks/useSeriesReducer";
+import { useMovie } from "../hooks/useMovie";
 
 export default function NewHero() {
-  const { isSeriesLoading, newSeason, seriesError, series } =
-    useSeriesReducer();
+  const { media, mediaInfo } = useMovie("2021", "2015", "2018", "");
 
   useEffect(() => {
-    if (newSeason) console.log(newSeason, "3");
-    if (series) console.log(series, "2");
-  }, [newSeason, series]);
+    console.log(media, "ee");
+  }, [mediaInfo]);
 }
--- src/hooks/useMovie.js
+++ src/hooks/useMovie.js
@@ -1,23 +1,44 @@
-import { useState, useEffect } from "react";
+import { useReducer, useEffect } from "react";
 import { useFetch } from "./useFetch";
 
-const BASE_MOVIE_URL = "https://api.themoviedb.org/3/movie/";
+const API_KEY = "70161bbcd895dec3c1b8d56d7c36b5fd";
 
-export function useMovie() {
-  const API_KEY = "70161bbcd895dec3c1b8d56d7c36b5fd";
+// INITIAL STATE
+const initialState = {
+  latestMedia: [],
+  allMedia: [],
+  mediaError: null,
+  isLoading: false,
+  media: [],
+};
+
+// REDUCER FUNCTION
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_ALL_MEDIA":
+      return { ...state, allMedia: action.payload };
+    case "SET_LATEST_MEDIA":
+      return { ...state, latestMedia: action.payload };
+    case "SET_MEDIA_ERROR":
+      return { ...state, mediaError: action.payload };
+    case "SET_LOADING":
+      return { ...state, isLoading: action.payload };
+    case "SET_MEDIA":
+      return { ...state, media: action.payload };
+    default:
+      return state;
+  }
+};
+
+/*  */
+export function useMovie(query) {
   const currentYear = new Date().getFullYear();
   const prevYear = currentYear - 10;
-  const [latestMedia, setLatestMedia] = useState([]);
-  const [allMedia, setAllMedia] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [mediaError, setMediaError] = useState(null);
-  const [media, setMedia] = useState([]);
-
-  //   Fetching the movies and series
+  ////////////////////FETCH MOVIES AND SERIES USING USEFETCH()
   const {
     data: moviesData,
     isPending: moviesLoading,
-    error,
+    error: moviesError,
   } = useFetch(
     `https://api.themoviedb.org/3/discover/movie/?primary_release_date.gte=${prevYear}-01-01&primary_release_date.lte=${currentYear}-12-31&sort_by=popularity.desc&api_key=${API_KEY}`
   );
@@ -28,7 +49,9 @@ export function useMovie() {
   } = useFetch(
     `https://api.themoviedb.org/3/discover/tv/?&include_video_language=en&first_air_date_year=2015&sort_by=popularity.desc&api_key=${API_KEY}`
   );
-  // combining data
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  ////////////////
   useEffect(() => {
     if (!seriesLoading && !moviesLoading) {
       const combinedData = [
@@ -45,64 +68,90 @@ export function useMovie() {
             }))
           : []),
       ];
-      setAllMedia(combinedData);
-      setIsLoading(false);
+      dispatch({ type: "SET_ALL_MEDIA", payload: combinedData });
+      dispatch({ type: "SET_LOADING", payload: false });
     }
-  }, [moviesData, moviesLoading, seriesData, seriesLoading]);
+  }, [moviesData, seriesData]);
 
-  //   Combining the Errors
   useEffect(() => {
-    if (error || seriesError) {
-      setMediaError(error || seriesError);
+    if (moviesError || seriesError) {
+      dispatch({
+        type: "SET_MEDIA_ERROR",
+        payload: moviesError || seriesError,
+      });
     }
-  }, []);
+  }, [moviesError, seriesError]);
 
   //   filtering those without backdrop
   useEffect(() => {
-    if (!isLoading && allMedia) {
-      const detail = allMedia?.filter((movie) => movie.backdrop_path !== null);
-      setLatestMedia(detail);
+    if (!state.isLoading && state.allMedia) {
+      const detail = state.allMedia?.filter(
+        (movie) => movie.backdrop_path !== null
+      );
+      dispatch({ type: "SET_LATEST_MEDIA", payload: detail });
     }
-  }, [isLoading, allMedia]);
+  }, [state.allMedia]);
 
-  //   getting id and movietype
   useEffect(() => {
-    if (!isLoading && latestMedia) {
-      const mediaInfo = latestMedia.map((movie) => ({
-        id: movie.id,
-        type: movie.media_type,
+    const controller = new AbortController();
+    if (!state.isLoading && state.latestMedia) {
+      const mediaInfo = state.latestMedia?.map((mov) => ({
+        id: mov.id,
+        type: mov.media_type,
       }));
-      console.log(latestMedia, "0");
+
       const fetchMovieData = async () => {
-        setIsLoading(true);
+        dispatch({ type: "SET_LOADING", payload: true });
+
         try {
-          const mediaPromise = mediaInfo?.map(({ id, type }) =>
+          const mediaPromise = mediaInfo.map(({ id, type }) =>
             fetch(
-              `https://api.themoviedb.org/3/${type}/${id}?api_key=${API_KEY}&append_to_response=videos`
+              `https://api.themoviedb.org/3/${type}/${id}?api_key=${API_KEY}&append_to_response=videos&${query}`,
+              { signal: controller.signal }
             )
           );
-
-          const mediaDetails = await Promise.all(
-            mediaPromise?.map((promise) => promise.then((res) => res.json()))
+          const mediaResponse = await Promise.all(
+            mediaPromise?.map((promise) =>
+              promise.then((res) => {
+                if (!res.ok) {
+                  throw new Error(res.statusText);
+                }
+                return res.json();
+              })
+            )
           );
-          const mediaDetailType = mediaDetails.map((data, i) => ({
+          const mediaDetails = mediaResponse.map((data, i) => ({
             ...data,
-            media_type: latestMedia[i].media_type,
+            media_type: state.latestMedia[i].media_type,
           }));
-          setIsLoading(false);
-          setMedia(mediaDetailType);
+
+          dispatch({ type: "SET_LOADING", payload: false });
+          dispatch({ type: "SET_MEDIA", payload: mediaDetails });
+
+          dispatch({ type: "SET_MEDIA_ERROR", payload: null });
         } catch (err) {
-          console.log(err.message);
+          if (err.name === "AbortError") {
+            dispatch({
+              type: "SET_MEDIA_ERROR",
+              payload: "the fetch was aborted",
+            });
+          } else {
+            dispatch({ type: "SET_LOADING", payload: false });
+            dispatch({
+              type: "SET_MEDIA_ERROR",
+              payload: "Could not fetch the data",
+            });
+          }
+
+          console.error(err);
         }
       };
       fetchMovieData();
+      return () => {
+        controller.abort();
+      };
     }
-  }, [moviesLoading, latestMedia]);
+  }, [state.latestMedia]);
 
-  return {
-    isLoading,
-    mediaError,
-    media,
-    API_KEY,
-  };
+  return { state, mediaInfo };
 }
